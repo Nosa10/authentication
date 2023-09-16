c91b84bbac17222e76fe31bed4d42cfb399565a8
Nosa10 <edisonnosakhare@gmail.com>
2023-09-15 22:19:24 -0700

    New vercel file

---
+++ src/components/Loading.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Loading.css'
+
+export default function Loading() {
+    return(
+       <div >
+        <div className="lds-spinner"><div></div><div></div>
+        <div></div><div></div><div></div><div>
+            </div><div></div><div></div><div>
+            </div><div></div><div></div><div></div></div>
+       </div>
+    )
+}
